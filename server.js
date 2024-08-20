bd48e421e0a849de88f364af5d2b5d3b3e294731
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-08-20 12:19:28 +0100

    fix to form, add step to mortgage rateindex.html

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ const app = express();
 const port = 8080;
 
 app.use(express.static("."));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/mortgagestats", (request, response) => {
+  const { mortgageAmount, mortgageTerm, mortgageRate, mortgageType } =
+    request.body;
+  const mortgageStats = {
+    loanAmount: mortgageAmount,
+    loanTerm: mortgageTerm,
+    loanRate: mortgageRate,
+    loanType: mortgageType,
+  };
+  console.log(mortgageStats);
+  response.send("Request successful");
+});
+
+// Do the keys in the original request body have to match the keys in the destructured object?
+//
 
 app.listen(port, () => {
   console.log("We're live");
